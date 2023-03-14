6316c2125f269c4de349530482415a0eb112dea7
sc-ah <aljoshua.hell@symblcrowd.de>
2023-03-14 12:05:12 +0100

    HtcLeoPkg: restructure header files

---
+++ HtcLeoPkg/Include/Library/ButtonsLib.h
@@ -0,0 +1,12 @@
+#ifndef __LIBRARY_QCOM_BUTTONS_LIB_H__
+#define __LIBRARY_QCOM_BUTTONS_LIB_H__
+
+//#include <Protocol/QcomClock.h>
+
+RETURN_STATUS
+EFIAPI
+MsmGpioButtonInitialize(VOID);
+
+//extern QCOM_CLOCK_PROTOCOL *gClock;
+
+#endif
