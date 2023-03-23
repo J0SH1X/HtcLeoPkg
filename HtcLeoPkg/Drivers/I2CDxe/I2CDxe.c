60df6b56414343509de97fb2eac3cc663baf3dc6
sc-ah <aljoshua.hell@symblcrowd.de>
2023-03-23 10:46:48 +0100

    HtcLeoPkg: I2cDxe: enable init

--- HtcLeoPkg/Drivers/I2CDxe/I2CDxe.c
+++ HtcLeoPkg/Drivers/I2CDxe/I2CDxe.c
@@ -46,11 +46,11 @@ I2CDxeInitialize(
 
 	DEBUG((EFI_D_ERROR, "I2CDxe init start\n"));
 
-	// Status = MsmI2cInitialize();
-  // ASSERT_EFI_ERROR(Status);
+	 Status = MsmI2cInitialize();
+   ASSERT_EFI_ERROR(Status);
 
-  // Status = MicroPInitialize();
-  // ASSERT_EFI_ERROR(Status);
+   Status = MicroPInitialize();
+   ASSERT_EFI_ERROR(Status);
 
 	DEBUG((EFI_D_ERROR, "I2CDxe init done!\n"));
   
