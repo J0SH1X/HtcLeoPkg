088f29c92b1ae49fc4c242cee62fa9e245751309
SC-AH <aljoshua.hell@symblcrowd.de>
2023-05-19 02:35:27 +0200

    KeyPadDebugDxe: port simplified gpio_keypad_init from lk button driver

--- HtcLeoPkg/Drivers/KeyPadDxe/KeyPadDxe.c
+++ HtcLeoPkg/Drivers/KeyPadDxe/KeyPadDxe.c
@@ -1,139 +1,121 @@
 #include <Uefi.h>
-
-#include <Library/BaseLib.h>
-#include <Library/MemoryAllocationLib.h>
-#include <Library/DebugLib.h>
-#include <Library/IoLib.h>
-#include <Library/PcdLib.h>
 #include <Library/UefiBootServicesTableLib.h>
-#include <Library/BaseMemoryLib.h>
+#include <Protocol/Timer.h>
+#include <Library/DebugLib.h>
+#include <Library/keypad.h>
 
-#include <Protocol/BlockIo.h>
-#include <Protocol/DevicePath.h>
+#define HTCLEO_GPIO_KP_MKOUT0    		33
+#define HTCLEO_GPIO_KP_MKOUT1    		32
+#define HTCLEO_GPIO_KP_MKOUT2    		31
+#define HTCLEO_GPIO_KP_MPIN0     		42
+#define HTCLEO_GPIO_KP_MPIN1     		41
+#define HTCLEO_GPIO_KP_MPIN2     		40
 
-#include <Chipset/iomap.h>
-#include <Chipset/clock.h>
-#include <Library/reg.h>
-#include <Library/adm.h>
-#include <Library/part.h>
-#include <Library/gpio.h>
+#define GPIO_INPUT	0x0000
+#define GPIO_OUTPUT	0x0001
 
-#include <Library/LKEnvLib.h>
+#define GPIO_LEVEL	0x0000
+#define GPIO_EDGE	0x0010
 
-const int rows = 3;
-const int cols = 3;
-int htcleo_row_gpios[] = { 33, 32, 31 };
-int htcleo_col_gpios[] = { 42, 41, 40 };
-int activeLow = 0x1 & 0x1;
-int keys[3][3] = {
-               {1,2,3},
-               {4,5,6},
-               {7,8,9}
+#define GPIO_RISING	0x0020
+#define GPIO_FALLING	0x0040
+
+#define GPIO_HIGH	0x0020
+#define GPIO_LOW	0x0040
+
+#define GPIO_PULLUP	0x0100
+#define GPIO_PULLDOWN	0x0200
+
+
+static uint32_t key_rowgpio[] = {
+	HTCLEO_GPIO_KP_MKOUT0,
+	HTCLEO_GPIO_KP_MKOUT1,
+	HTCLEO_GPIO_KP_MKOUT2,
 };
 
-VOID initKeyPad() {
-	DEBUG((EFI_D_ERROR, "Keypad init Started!\n"));
-	mdelay(3000);
-	// set all cols low
-	// gpio_set(htcleo_col_gpios[0], GPIO_LOW);
-	// gpio_set(htcleo_col_gpios[1], GPIO_LOW);
-	// gpio_set(htcleo_col_gpios[2], GPIO_LOW);
-	for (int i = 0; i < ARRAY_SIZE(htcleo_col_gpios); i++){
-		gpio_set(htcleo_row_gpios[i], GPIO_LOW);
+static uint32_t key_colgpio[] = {
+	HTCLEO_GPIO_KP_MPIN0,
+	HTCLEO_GPIO_KP_MPIN1,
+	HTCLEO_GPIO_KP_MPIN2,
+};
+#define ASZ(x) ARRAY_SIZE(x)
+
+typedef enum {
+  KEY_VOLUMEUP,
+  KEY_VOLUMEDOWN,
+  KEY_SOFT1,
+  KEY_SEND,
+  KEY_CLEAR,
+  KEY_BACK,
+  KEY_HOME,
+} HTCLEO_BUTTON_TYPES;
+
+#define KEYMAP_INDEX(row, col) ((row)*ARRAY_SIZE(key_colgpio) + (col))
+static uint16_t htcleo_keymap[ASZ(key_colgpio) * ASZ(key_rowgpio)] = {
+	[KEYMAP_INDEX(0, 0)] = KEY_VOLUMEUP,	// Volume Up
+	[KEYMAP_INDEX(0, 1)] = KEY_VOLUMEDOWN,	// Volume Down
+	[KEYMAP_INDEX(1, 0)] = KEY_SOFT1,		// Windows Button
+	[KEYMAP_INDEX(1, 1)] = KEY_SEND,		// Dial Button
+	[KEYMAP_INDEX(1, 2)] = KEY_CLEAR,		// Hangup Button
+	[KEYMAP_INDEX(2, 0)] = KEY_BACK,		// Back Button
+	[KEYMAP_INDEX(2, 1)] = KEY_HOME,		// Home Button
+};
+#undef KEYMAP_INDEX
+
+void htcleo_handle_key_bkl(){
+	//handle button backlight here
+}
+static struct gpio_keypad_info htcleo_keypad_info = {
+	.keymap        	= htcleo_keymap,
+	.output_gpios  	= key_rowgpio,
+	.input_gpios   	= key_colgpio,
+	.noutputs      	= ASZ(key_rowgpio),
+	.ninputs      	= ASZ(key_colgpio),
+	.settle_time   	= 40,
+	.poll_time     	= 20,
+	.flags        	= GPIOKPF_DRIVE_INACTIVE,
+	.notify_fn     	= &htcleo_handle_key_bkl,
+};
+#undef ASZ
+
+EFI_EVENT TimerEvent;
+EFI_TIMER_ARCH_PROTOCOL *TimerProtocol;
+
+
+void gpio_keypad_init(struct gpio_keypad_info *kpinfo)
+{ 
+	int output_val;
+	int output_cfg;
+	int i;
+
+	output_val = (!!(kpinfo->flags & GPIOKPF_ACTIVE_HIGH))^(!!(kpinfo->flags & GPIOKPF_DRIVE_INACTIVE));
+	output_cfg = kpinfo->flags & GPIOKPF_DRIVE_INACTIVE ? GPIO_OUTPUT : 0;
+	for (i = 0; i < kpinfo->noutputs; i++) {
+		gpio_set(kpinfo->output_gpios[i], output_val);
+		gpio_config(kpinfo->output_gpios[i], output_cfg);
 	}
-	DEBUG((EFI_D_ERROR, "Keyboard Col Pins set to LOW!\n"));
-	mdelay(3000);
-	DEBUG((EFI_D_ERROR, "Exiting 'INIT_KeyPad' function\n"));
-	mdelay(3000);
+	for (i = 0; i < kpinfo->ninputs; i++) {
+		gpio_config(kpinfo->input_gpios[i], GPIO_INPUT);
+	}
+}
+
+EFI_STATUS KeyPadDxeInitialize(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+
+    DEBUG((EFI_D_ERROR, "KEYPADDXE START\n"));
+
+
+	gpio_keypad_init(&htcleo_keypad_info);
+
+	gpio_set(HTCLEO_GPIO_KP_MKOUT1,1);
+	while (true){
+		int status = gpio_get(HTCLEO_GPIO_KP_MPIN1);
+		 DEBUG((EFI_D_ERROR, "HTCLEO_GPIO_KP_MPIN1 is: %d\n", status));
+		 mdelay(1000);
+	}
+
+    
+
+    return EFI_SUCCESS;
 }
 
 
-
-UINT8 getKey() {
-	DEBUG((EFI_D_ERROR, "Entering 'getKey' function!\n"));
-	int k = 0;
-
-	for(int i = 0; i < ARRAY_SIZE(htcleo_col_gpios); i++){
-   	     gpio_set(htcleo_col_gpios[i], GPIO_LOW);
-        	for(int j = 0; j < ARRAY_SIZE(htcleo_row_gpios); j++){
-				int returnValue2 = gpio_get(htcleo_row_gpios[j]);
-				DEBUG((EFI_D_ERROR, "returnValue2 is: %d!\n", returnValue2));
-            		if(returnValue2 == GPIO_LOW){
-            			mdelay(20);    //20ms debounce time
-						int returnValue = gpio_get(htcleo_row_gpios[j]);
-						DEBUG((EFI_D_ERROR, "returnValue is: %s!\n", returnValue));
-            			while(returnValue == GPIO_LOW);
-            			k = keys[j][i];
-            		}else {
-						DEBUG((EFI_D_ERROR, "GPIO was high!\n"));
-					}
-        	 }
-   	gpio_set(htcleo_col_gpios[i], GPIO_HIGH); 
-      	}
-      	return k;
-}
-
-
-
-RETURN_STATUS
-EFIAPI
-KeyPadDxeInitialize(  IN EFI_HANDLE         ImageHandle,
-  IN EFI_SYSTEM_TABLE   *SystemTable)
-{
-	EFI_STATUS  Status = EFI_SUCCESS;
-
-	DEBUG((EFI_D_ERROR, "Keypad init start\n"));
-
-	initKeyPad();
-
-// 	for(;;) {
-// 	int key = getKey();
-// 	if(key != 0){
-// 		DEBUG((EFI_D_ERROR, "Key Index: %d\n", key));
-// 	}else {
-// 		DEBUG((EFI_D_ERROR, "key is null \n"));
-// 	}
-// 	mdelay(1000);
-// }
-
-// for (;;){
-// 	int status = gpio_get(94);
-// 	DEBUG((EFI_D_ERROR, "status of gpio 94 is: %d\n", status));
-// 		// int status3 = gpio_get(33);
-// 	// DEBUG((EFI_D_ERROR, "status of gpio 33 is: %d\n", status3));
-// 	// 		int status4 = gpio_get(42);
-// 	// DEBUG((EFI_D_ERROR, "status of gpio 33 is: %d\n", status4));
-// 	mdelay(1000);
-// }
-
-//check volume up button code
-while true{
-// for (int i = 0; i < ARRAY_SIZE(htcleo_row_gpios); i++){
-// 	gpio_set(htcleo_row_gpios[i], 1);
-// 	for (int j = 0; j < ARRAY_SIZE(htcleo_col_gpios); i++){
-// 		int status = gpio_get(htcleo_col_gpios[i]);
-// 		DEBUG((EFI_D_ERROR, "gpio status is: %d!\n", status));
-// 		mdelay(500);
-// 		bool isPressed = (status ? 1 : 0) ^ activeLow;
-
-// 	}
-// }
-
-// gpio_set(33,1);
-// int status = gpio_get(94);
-// DEBUG((EFI_D_ERROR, "GPIO 94 is: %d!\n", status));
-// if (status == 0){
-// 	gpio_set(48,1);
-// }else {
-// 	gpio_set(48,0);
-// }
-
-KeypadDeviceImplConstructor();
-KeypadDeviceImplGetKeys();
-
-}
-
-	DEBUG((EFI_D_ERROR, "Keypad init done!\n"));
-
-	return Status;
-}
