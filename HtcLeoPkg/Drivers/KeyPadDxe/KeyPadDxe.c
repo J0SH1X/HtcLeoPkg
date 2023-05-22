724b789d0073e148e233f2f7bf2e6ca03e79bb5e
SC-AH <aljoshua.hell@symblcrowd.de>
2023-05-23 00:11:02 +0200

    Fix Button poll logic

--- HtcLeoPkg/Drivers/KeyPadDxe/KeyPadDxe.c
+++ HtcLeoPkg/Drivers/KeyPadDxe/KeyPadDxe.c
@@ -13,16 +13,6 @@
 
 #define GPIO_INPUT	0x0000
 #define GPIO_OUTPUT	0x0001
-
-#define GPIO_LEVEL	0x0000
-#define GPIO_EDGE	0x0010
-
-#define GPIO_RISING	0x0020
-#define GPIO_FALLING	0x0040
-
-#define GPIO_HIGH	0x0020
-#define GPIO_LOW	0x0040
-
 #define GPIO_PULLUP	0x0100
 #define GPIO_PULLDOWN	0x0200
 
@@ -40,27 +30,24 @@ static uint32_t key_colgpio[] = {
 };
 #define ASZ(x) ARRAY_SIZE(x)
 
-typedef enum {
-  KEY_VOLUMEUP,
-  KEY_VOLUMEDOWN,
-  KEY_SOFT1,
-  KEY_SEND,
-  KEY_CLEAR,
-  KEY_BACK,
-  KEY_HOME,
-} HTCLEO_BUTTON_TYPES;
 
-#define KEYMAP_INDEX(row, col) ((row)*ARRAY_SIZE(key_colgpio) + (col))
-static uint16_t htcleo_keymap[ASZ(key_colgpio) * ASZ(key_rowgpio)] = {
-	[KEYMAP_INDEX(0, 0)] = KEY_VOLUMEUP,	// Volume Up
+
+
+#define KEYMAP_INDEX(row, col) ((row)*3 + (col))
+static HTCLEO_BUTTON_TYPE htcleo_keymap[3 * 3] = {
+	[KEYMAP_INDEX(0, 0)] = KEY_BACK,		//  Back Button again, mapped
 	[KEYMAP_INDEX(0, 1)] = KEY_VOLUMEDOWN,	// Volume Down
-	[KEYMAP_INDEX(1, 0)] = KEY_SOFT1,		// Windows Button
-	[KEYMAP_INDEX(1, 1)] = KEY_SEND,		// Dial Button
+	[KEYMAP_INDEX(1, 0)] = KEY_BACK,		// Back Button, mapped
+	[KEYMAP_INDEX(1, 1)] = KEY_HOME,		// Home Button
 	[KEYMAP_INDEX(1, 2)] = KEY_CLEAR,		// Hangup Button
-	[KEYMAP_INDEX(2, 0)] = KEY_BACK,		// Back Button
-	[KEYMAP_INDEX(2, 1)] = KEY_HOME,		// Home Button
+	[KEYMAP_INDEX(2, 0)] = KEY_SOFT1,		// Windows Button, mapped
+	[KEYMAP_INDEX(2, 1)] = KEY_SEND,		// Dial Button , mapped
 };
-#undef KEYMAP_INDEX
+
+// HTCLEO_BUTTON_TYPE leoKeys[9] = {
+// 	KEY_BACK,
+// 	K
+// }
 
 void htcleo_handle_key_bkl(){
 	//handle button backlight here
@@ -90,8 +77,11 @@ void gpio_keypad_init(struct gpio_keypad_info *kpinfo)
 
 	output_val = (!!(kpinfo->flags & GPIOKPF_ACTIVE_HIGH))^(!!(kpinfo->flags & GPIOKPF_DRIVE_INACTIVE));
 	output_cfg = kpinfo->flags & GPIOKPF_DRIVE_INACTIVE ? GPIO_OUTPUT : 0;
+
 	for (i = 0; i < kpinfo->noutputs; i++) {
+		DEBUG((EFI_D_ERROR, "Setting GPIO %d to %d\n",kpinfo->output_gpios[i], output_val ));
 		gpio_set(kpinfo->output_gpios[i], output_val);
+		DEBUG((EFI_D_ERROR, "Config GPIO %d to %d\n",kpinfo->output_gpios[i], output_cfg ));
 		gpio_config(kpinfo->output_gpios[i], output_cfg);
 	}
 	for (i = 0; i < kpinfo->ninputs; i++) {
@@ -104,15 +94,30 @@ EFI_STATUS KeyPadDxeInitialize(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemT
     DEBUG((EFI_D_ERROR, "KEYPADDXE START\n"));
 
 
-	gpio_keypad_init(&htcleo_keypad_info);
+	//gpio_keypad_init(&htcleo_keypad_info);
+	while (TRUE){
+		for (int i = 0; i < 3; i++){
+			//DEBUG((EFI_D_ERROR, "setting GPIO %d to %d\n", key_rowgpio[i], 1));
+			gpio_set(key_rowgpio[i], 1);
+				for (int j = 0; j < 3; j++){
+					int status = gpio_get(key_colgpio[j]);
 
-	gpio_set(HTCLEO_GPIO_KP_MKOUT1,1);
-	while (true){
-		int status = gpio_get(HTCLEO_GPIO_KP_MPIN1);
-		 DEBUG((EFI_D_ERROR, "HTCLEO_GPIO_KP_MPIN1 is: %d\n", status));
-		 mdelay(1000);
+					if (status == 0){
+						HTCLEO_BUTTON_TYPE button = htcleo_keymap[KEYMAP_INDEX(i,j)];
+						//DEBUG((EFI_D_ERROR, "button pressed is %s\n", button));
+
+						if (button == KEY_HOME){
+							DEBUG((EFI_D_ERROR, "button pressed, i: %d, j: %d\n", i, j));
+						}
+					}else {
+					//DEBUG((EFI_D_ERROR, "GPIO %d is %d\n", key_colgpio[j], status));
+					}
+				}
+			gpio_set(key_rowgpio[i], 0);
+
+		}
+		mdelay(3000);
 	}
-
     
 
     return EFI_SUCCESS;
