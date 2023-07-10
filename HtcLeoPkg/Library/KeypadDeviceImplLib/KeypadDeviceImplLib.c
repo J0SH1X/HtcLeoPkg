38b0dcce62fbe817853bc57567a2e2674516171c
SC-AH <aljoshua.hell@symblcrowd.de>
2023-07-10 15:32:13 +0200

    Buttons: cleanup and configure all buttons

--- HtcLeoPkg/Library/KeypadDeviceImplLib/KeypadDeviceImplLib.c
+++ HtcLeoPkg/Library/KeypadDeviceImplLib/KeypadDeviceImplLib.c
@@ -64,13 +64,13 @@ KEY_CONTEXT_PRIVATE *KeypadKeyCodeToKeyContext(UINT32 KeyCode)
     return &KeyContextVolumeUp;
   else if (KeyCode == 116)
     return &KeyContextPower;
-  else if (KeyCode == 116)
-    return &KeyContextBack;
   else if (KeyCode == 117)
-    return &KeyContextWindows;
+    return &KeyContextBack;
   else if (KeyCode == 118)
-    return &KeyContextHome;
+    return &KeyContextWindows;
   else if (KeyCode == 119)
+    return &KeyContextHome;
+  else if (KeyCode == 120)
     return &KeyContextDial;
   else
     return NULL;
@@ -97,7 +97,7 @@ KeypadDeviceImplConstructor(VOID)
   StaticContext->ActiveLow  = 0x1 & 0x1;
   StaticContext->IsValid    = TRUE;
 
-  // back button
+    // volume up button
   StaticContext             = KeypadKeyCodeToKeyContext(116);
   StaticContext->DeviceType = KEY_DEVICE_TYPE_KEYMATRIX;
   StaticContext->GpioOut    = 33;
@@ -105,12 +105,48 @@ KeypadDeviceImplConstructor(VOID)
   StaticContext->ActiveLow  = 0x1 & 0x1;
   StaticContext->IsValid    = TRUE;
 
-  // power button button again replace this with the rest of the keymap
-  // StaticContext             = KeypadKeyCodeToKeyContext(116);
-  // StaticContext->DeviceType = KEY_DEVICE_TYPE_LEGACY;
-  // StaticContext->Gpio       = 94;
-  // StaticContext->ActiveLow  = 0x1 & 0x1;
-  // StaticContext->IsValid    = TRUE;
+    // volume down button
+  StaticContext             = KeypadKeyCodeToKeyContext(116);
+  StaticContext->DeviceType = KEY_DEVICE_TYPE_KEYMATRIX;
+  StaticContext->GpioOut    = 33;
+  StaticContext->GpioIn     = 41;
+  StaticContext->ActiveLow  = 0x1 & 0x1;
+  StaticContext->IsValid    = TRUE;
+
+  // back button
+  StaticContext             = KeypadKeyCodeToKeyContext(117);
+  StaticContext->DeviceType = KEY_DEVICE_TYPE_KEYMATRIX;
+  StaticContext->GpioOut    = 31;
+  StaticContext->GpioIn     = 42;
+  StaticContext->ActiveLow  = 0x1 & 0x1;
+  StaticContext->IsValid    = TRUE;
+
+  // windows button
+  StaticContext             = KeypadKeyCodeToKeyContext(118);
+  StaticContext->DeviceType = KEY_DEVICE_TYPE_KEYMATRIX;
+  StaticContext->GpioOut    = 32;
+  StaticContext->GpioIn     = 42;
+  StaticContext->ActiveLow  = 0x1 & 0x1;
+  StaticContext->IsValid    = TRUE;
+
+    // home button
+  StaticContext             = KeypadKeyCodeToKeyContext(119);
+  StaticContext->DeviceType = KEY_DEVICE_TYPE_KEYMATRIX;
+  StaticContext->GpioOut    = 31;
+  StaticContext->GpioIn     = 41;
+  StaticContext->ActiveLow  = 0x1 & 0x1;
+  StaticContext->IsValid    = TRUE;
+
+
+    // dial button
+  StaticContext             = KeypadKeyCodeToKeyContext(120);
+  StaticContext->DeviceType = KEY_DEVICE_TYPE_KEYMATRIX;
+  StaticContext->GpioOut    = 32;
+  StaticContext->GpioIn     = 41;
+  StaticContext->ActiveLow  = 0x1 & 0x1;
+  StaticContext->IsValid    = TRUE;
+
+
 
   return RETURN_SUCCESS;
 }
@@ -154,11 +190,10 @@ EFI_STATUS KeypadDeviceImplGetKeys(
 {
   UINT8   GpioStatus;
   BOOLEAN IsPressed;
-  INTN    RC;
   UINTN   Index;
   DEBUG((EFI_D_ERROR, "KeypadDeviceImplGetKeys!\n"));
 
-  for (Index = 0; Index < ARRAY_SIZE(KeyList); Index++) {
+  for (Index = 0; Index < (sizeof(KeyList) / sizeof(KeyList[0])); Index++) {
     KEY_CONTEXT_PRIVATE *Context = KeyList[Index];
 
     // check if this is a valid key
@@ -166,47 +201,26 @@ EFI_STATUS KeypadDeviceImplGetKeys(
       continue;
 
     // get status
-    if (Context->DeviceType == KEY_DEVICE_TYPE_TLMM) {
-      // GpioStatus = gGpioTlmm->Get(Context->Gpio);
-      RC = 0;
-    }
-    else if (Context->DeviceType == KEY_DEVICE_TYPE_PM8X41) {
-      // RC = gPm8x41->pm8x41_gpio_get(Context->Gpio, &GpioStatus);
-    }
-    else if (Context->DeviceType == KEY_DEVICE_TYPE_PM8X41_PON) {
-      if (Context->PonType == 0x1) {
-        //  GpioStatus = gPm8x41->pm8x41_resin_status();
-      }
-      else if (Context->PonType == 0x0) {
-        //  GpioStatus = gPm8x41->pm8x41_get_pwrkey_is_pressed();
-      }
-      else
-        continue;
-
-      RC = 0;
-    }
-    else if (Context->DeviceType == KEY_DEVICE_TYPE_LEGACY) {
+     if (Context->DeviceType == KEY_DEVICE_TYPE_LEGACY) {
       // impliement hd2 gpio shit here
       GpioStatus = gpio_get(Context->Gpio);
       DEBUG((EFI_D_ERROR, "LEGACY KEYTYPE GPIO STATUS: %d!\n", GpioStatus));
       gpio_set(48, 1);
-      RC = -1;
     }
     else if (Context->DeviceType == KEY_DEVICE_TYPE_KEYMATRIX) {
       DEBUG((EFI_D_ERROR, "KEYMATRIX!\n"));
       gpio_set(Context->GpioOut, 0);
       GpioStatus = gpio_get(Context->GpioIn);
-      RC         = -1;
     }
     else {
       continue;
     }
-    if (RC != 0)
-      continue;
 
     // update key status
     // 0000 ^0001 = 0001 = decimal 1
     IsPressed = (GpioStatus ? 1 : 0) ^ Context->ActiveLow;
+
+    DEBUG((EFI_D_ERROR, "IsPressed var is: %d!\n", IsPressed));
 
     if (IsPressed) {
       // impl a timed callback here that enables gpio 48 for a few secs and then
